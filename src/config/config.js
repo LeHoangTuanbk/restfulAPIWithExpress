e12848c8c4a14d8c93b10ca45c0931bfac23a811
Le Hoang Tuan <lehoangtuan@Les-Laptop.local>
2023-08-25 15:15:05 +0900

    add env.example

--- src/config/config.js
+++ src/config/config.js
@@ -10,14 +10,15 @@ module.exports = {
     "dialect": "postgres",
     "port": process.env.DB_PORT_DEV
   },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  },
   "production": {
+    "username": process.env.DB_USERNAME_PRO,
+    "password": process.env.DB_PASSWORD_PRO,
+    "database": process.env.DB_NAME_PRO,
+    "host": process.env.DB_HOST_PRO,
+    "dialect": "postgres",
+    "port": process.env.DB_PORT_PRO
+  },
+  "test": {
     "username": "root",
     "password": null,
     "database": "database_production",
